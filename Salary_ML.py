d46fd1799180567d00b1e9ea4737516f174f6829
laioespinheira <laioespinheira@gmail.com>
2022-08-13 14:24:29 +0000

    changed import seaborn order

--- Salary_ML.py
+++ Salary_ML.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_squared_error
-import seaborn as sns
+
 
 def clean_data(df):
     '''
